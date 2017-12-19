be7c77f4ea458bf864a9de027f441b45b993507a
Rogoths <r.muny@hotmail.fr>
2017-12-19 16:54:56 +0100

    generation du labyrinthe en liste

--- Macgyver.py
+++ Macgyver.py
@@ -1,32 +1,38 @@
-class niveau:
+class Niveau:
 #générer le labyrinthe à partir du fichier lab1
-    def __init__(self):
 
-    def generer():
-        with open("lab1") as fichier:
-            labyrinthe = fichier.readlines()
+    def __init__(self):
+        print("Affichage du labyrinthe")
+        with open("lab1", "r") as self.structure:
+            self.ouverture_fichier = self.structure.read()
+            self.generation_liste = list(self.ouverture_fichier)
+
+labyrinthe = Niveau()
+print(labyrinthe.generation_liste)
+
+
 
 
 #afficher ce labyrinthe
-print(labyrinthe)
+#print(self.fichier())
 #localiser macgyver sur le labyrinthe
-"""if "m" dans la liste alors position du joueur"""
+#"""if "m" dans la liste alors position du joueur"""
 #macgyver peut mourir
-"""if déplacement "m" sur position liste m+1, m-1, m+15, m-15 == "g" sans les objets alors game over"""
+#"""if déplacement "m" sur position liste m+1, m-1, m+15, m-15 == "g" sans les objets alors game over"""
 #faire déplacer MacGyver avec les touches directionnelles
- """if "m" dans la liste récupérer sa position dans la liste"""
- """if les positions dans la liste m+1, m-1, m+15, m-15 == "0" alors peut se déplacer"""
- """if les positions dans la liste m+1, m-1, m+15, m-15 == "w" alors déplacement impossible"""
- """remplacer la position de "m" dans la liste par une nouvelle"""
+ #"""if "m" dans la liste récupérer sa position dans la liste"""
+ #"""if les positions dans la liste m+1, m-1, m+15, m-15 == "0" alors peut se déplacer"""
+ #"""if les positions dans la liste m+1, m-1, m+15, m-15 == "w" alors déplacement impossible"""
+ #"""remplacer la position de "m" dans la liste par une nouvelle"""
 #macgyver doit récupérer des objets pour sortir
-"""placer aléatoirement dans la liste du labyrinthe les objets"""
-"""récupérer les objets est les mettre dans un inventaire(afficher le nombre d'objet)"""
-"""faire disparaître les objets de la liste"""
+#"""placer aléatoirement dans la liste du labyrinthe les objets"""
+#"""récupérer les objets est les mettre dans un inventaire(afficher le nombre d'objet)"""
+#"""faire disparaître les objets de la liste"""
 
 #localiser le gardien sur le labyrinthe
 
 #terminer la partie
-"""if déplacement "m" sur position liste m+1, m-1, m+15, m-15 == "g" avec les objets alors gagné sinon perdu"""
+#"""if déplacement "m" sur position liste m+1, m-1, m+15, m-15 == "g" avec les objets alors gagné sinon perdu"""
 
 #class macgyver: # class permettant de créer le personnage
 
